4a95e96cc53f1c2ea62719edd72e15d359ff7ef0
Chris <security4u@gmail.com>
2021-07-15 14:13:15 +0900

    Create syslog.py

---
+++ syslog.py
@@ -0,0 +1,9 @@
+
+f = open('log_NAS_file', 'r', encoding='UTF8')
+
+lines = f.readlines()
+for line in lines:
+    lines[1].split()
+    file = (lines)
+    log = file[2].split(' ')
+    print(log[0:2], log[4:5], log[12], log[18:22])
